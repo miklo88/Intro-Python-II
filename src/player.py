c92f7a2aefa0a0c993cb0384b9351a5fb35682f4
Carl Redding <credding07@gmail.com>
2020-06-10 23:21:46 -0400

    was loosin my mind over some whitespace that I didn't have indentation for. Having fun elaborating on my character but i'll keep it short and sweet and continue to move on. Set up some HP for some future battles. Going to move over and set up my classes for the rooms really quick

--- src/player.py
+++ src/player.py
@@ -1,2 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, current_room):
+        self.current_room = current_room
+    
+
+class Attributes(Player):
+    def __init__(self, name, hp, current_room):
+        super().__init__(current_room)
+        self.name = name
+        self.hp = hp
+
+    def __str__(self):
+        return f'Hola mis amigos! My name is {self.name}. Seems like this {self.current_room} is kind of spooky.'
+
+player1 = Attributes('Carlitos', 100, 'Main Room')
+print(player1)
+# print(player1.name)
+print(player1.hp)
