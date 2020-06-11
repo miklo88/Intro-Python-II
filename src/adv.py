8202374f3d6230b13b64f42a5ad6579af0386242
Carl Redding <credding07@gmail.com>
2020-06-11 17:41:18 -0400

    referred back to the video and commented out the list and the dictionary so just working with variables for now and have the logic built up for the quest to begin and start. now just have to clean up moving from room to room.

--- src/adv.py
+++ src/adv.py
@@ -2,64 +2,119 @@ from room import Room
 from player import Player
 
 # Declare all the rooms
+#room dictionary with attributes/properites of name and description.
+# room = {
+#     'outside':  Room("Outside Cave Entrance",
+#                      "North of you, the cave mount beckons"),
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+# passages run north and east."""),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+# to north. The smell of gold permeates the air."""),
+
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south."""),
+# }
+# #room variables
+outside = Room("Outside Cave Entrance",
+"North of you, the cave mount beckons")
+
+foyer = Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east.""")
+
+overlook = Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""")
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+narrow = Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""")
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+treasure = Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
+earlier adventurers. The only exit is to the south.""")
+#welcome instructions
+instructions = """Welcome to find the treasure! As a player you will navigate multiple rooms using the cardinal
+directions n for north, s for south, e for east, w for west to find the treasure!"""
+print(instructions)
+#cardinal direction choice
+#Read the user input (The python commmands)
+# direction_choice = input(f'Begin your quest now: ')
+#Evaluate your code (to work out what I mean)
+# chosen_path = 
+#Print any results (so you can see the comps response)
+# print(chosen_path)
+# print(direction_choice)
+#Loop back to step 1 (continue the conversation)
 
-
-# Link rooms together
-
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
-
-
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['overlook'].s_to = room['foyer']
+# room['foyer'].e_to = room['narrow']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
+outside.n_to = foyer
+foyer.s_to = outside
+foyer.n_to = overlook
+overlook.s_to = foyer
+foyer.e_to = narrow
+narrow.w_to = foyer
+narrow.n_to = treasure
+treasure.s_to = narrow
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
-
-# class test that data is flowing.
-# print(type(room)) #dictionary data type.
-# print(room.keys()) # getting keys LIST of room name 
-# print(room.values()) # getting values LIST of objects 
-print(room['foyer'])# returns name and description.
-print(room['outside'])# returns name and description.
-print(room['overlook'])# returns name and description.
-print(room['narrow'])# returns name and description.
-print(room['treasure'])# returns name and description.
-
-# print(player)
-
+# print(room['outside'])
+player = Player()
+player.current_room = outside
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+while True:
+    # * Prints the current room name
+    print(player.current_room)
+    # print(player.current_room.name)
+    # * Prints the current description (the textwrap module might be useful here).
+    # print(player.current_room.description)
+    # * Waits for user input and decides what to do.
+    input_var = input("submit a command: " )
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    #
+    # If the user enters "q", quit the game.
+    if input_var == 'n':
+        #check if the current rooms has a n_to attribute
+        if player.current_room.n_to is not None:
+         #move the player to that room
+            player.current_room == player.current_room.n_to
+    elif input_var == 's':
+        if player.current_room.s_to is not None:
+            #move the player to that room
+            player.current_room == player.current_room.s_to
+    elif input_var == 'e':
+        if player.current_room.e_to is not None:
+            #move the player to that room
+            player.current_room == player.current_room.e_to
+    elif input_var == 'w':
+        if player.current_room.w_to is not None:
+            #move the player to that room
+            player.current_room == player.current_room.w_to
+#to break out of the game
+    elif input_var == 'q':
+        print(f'The quest has ended.') 
+        break
+# #for any commands other than n, s, e, w
+#     elif input_var != 'n, s, e, w':
+#         print('Wrong command, try again')
+    else:
+        print('Wrong command, try again')
+#to break out of the game
+# elif input_var == 'q'
+#elif q:
+    # break
--- src/room.py
+++ src/room.py
@@ -1,21 +1,32 @@
+# room blueprint - a room name, the description. extra sugar, add items into this class.
+# a category to choose a room by is a cardinal direction.
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-    #room description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None 
+        self.w_to = None
+    #room name and description from obj to str func
     def __str__(self):
-        return f'{self.name} {self.description}'
+        output = f'{self.name}: {self.description} \n'
+        if self.n_to:
+            output += 'To the north is: ' + self.n_to.name + '\n'
+        if self.s_to:
+            output += 'To the south is: ' + self.s_to.name + '\n'
+        if self.e_to:
+            output += 'To the east is: ' + self.e_to.name + '\n'
+        if self.w_to:
+            output += 'To the west is: ' + self.w_to.name + '\n'
+        
+        return output
+    #repr function for easier debugging.
+    # def __repr__(self):
+    #     return f'{self.name} ; {self.description}'
 
-# #another class of room to make sure my class here is working
-# class Description(Room):
-#     def __init__(self, name, description):
-#         super().__init__(name)
-#         self.description = description
-
-#     def __str__(self):
-#         return f'Uuu I am a {self.description} {self.name}!'
-
+#test
 # room = Room('Cellar', 'stinky smelly Moldy')
 # print(room)
